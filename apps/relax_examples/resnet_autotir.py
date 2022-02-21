2d7693dcbf897fcf66e120386fe7655cfee08da4
YuchenJin <yuchenj@cs.washington.edu>
2022-02-21 12:49:07 -0800

    update

--- apps/relax_examples/resnet_autotir.py
+++ apps/relax_examples/resnet_autotir.py
@@ -24,6 +24,7 @@ from tvm.relax.testing import relay_translator, nn
 from tvm.runtime import vm as vm_rt
 from tvm.script import relax as R
 import numpy as np
+import os
 import tempfile
 import time
 from typing import List
@@ -40,37 +41,77 @@ from tvm.meta_schedule.builder import LocalBuilder
 from tvm.meta_schedule.runner import LocalRunner
 
 
-if __name__ == "__main__":
-    relay_mod, _ = testing.resnet.get_workload(num_layers=50, batch_size=1, dtype="float32")
+def autotir_tune(batch_size, target, database, is_tune, layout="NHWC", dtype="float32"):
+    if layout == "NHWC":
+        image_shape = (224, 224, 3)
+    elif layout == "NCHW":
+        image_shape = (3, 224, 224)
 
-    # translate the ResNet model from Relay to Relax
+    relay_mod, params = testing.resnet.get_workload(
+        num_layers=50,
+        batch_size=batch_size,
+        dtype=dtype,
+        layout=layout,
+        image_shape=image_shape,
+    )
+
     relax_mod = relay_translator.from_relay(relay_mod["main"])
 
-    database = JSONDatabase(
-        path_workload="./workload2.json",
-        path_tuning_record="./tuning_record2.json",
-    )
-    target = tvm.target.Target("llvm --num-cores=16")
+    if is_tune:
+        print("tuning")
+        runner = LocalRunner()
+        builder = LocalBuilder()
+        tasks = extract_task_from_relax(relax_mod, target=target)
+        for task in tasks:
+            print(f"Extracted task: {task.task_name}, {task.target}")
+            with tempfile.TemporaryDirectory() as work_dir:
+                sch = tune_tir(
+                    mod=task.mod,
+                    target=target,
+                    config=ReplayTraceConfig(
+                        num_trials_per_iter=32,
+                        num_trials_total=2000,
+                    ),
+                    builder=builder,
+                    runner=runner,
+                    work_dir=work_dir,
+                    database=database,
+                    num_threads=16,
+                )
+    return relay_mod, relax_mod
 
-    # runner = LocalRunner()
-    # builder = LocalBuilder()
-    # tasks = extract_task_from_relax(relax_mod, target=target)
-    # for task in tasks:
-    #     print(f"Extracted task: {task.task_name}, {task.target}")
-    #     with tempfile.TemporaryDirectory() as work_dir:
-    #         sch = tune_tir(
-    #             mod=task.mod,
-    #             target=target,
-    #             config=ReplayTraceConfig(
-    #                 num_trials_per_iter=32,
-    #                 num_trials_total=2000,
-    #             ),
-    #             builder=builder,
-    #             runner=runner,
-    #             work_dir=work_dir,
-    #             database=database,
-    #             num_threads=16,
-    #         )
+
+if __name__ == "__main__":
+    target = tvm.target.Target("llvm -mcpu=core-avx2 --num-cores=16")
+    network = "resnet-50"
+    batch_size = 1
+    layout = "NCHW"
+    # layout = "NHWC"
+    dtype = "float32"
+
+    workload_file = "autotir_logs/" + "%s-%s-B%d-%s-workload.json" % (
+        network,
+        layout,
+        batch_size,
+        target.kind.name,
+    )
+
+    tuning_record_file = "autotir_logs/" + "%s-%s-B%d-%s-tuning_record.json" % (
+        network,
+        layout,
+        batch_size,
+        target.kind.name,
+    )
+
+    os.makedirs(os.path.dirname(workload_file), exist_ok=True)
+    is_tune = not os.path.exists(workload_file)
+
+    database = JSONDatabase(
+        path_workload=workload_file,
+        path_tuning_record=tuning_record_file,
+    )
+
+    relay_mod, relax_mod = autotir_tune(batch_size, target, database, is_tune, layout, dtype)
 
     # resnet benchmarking
     with transform.PassContext(opt_level=3):
